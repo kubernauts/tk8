739dfffa47101601e9874f65c4dd8416320a9f81
Christopher Adigun <futuredon@gmail.com>
2018-03-26 19:42:43 +0200

    Added openstack module

--- main.go
+++ main.go
@@ -11,11 +11,10 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// ...
 
 package main
 
-import "github.com/kubernauts/tk8/cmd"
+import "github.com/infinitydon/tk8/cmd"
 
 func main() {
 	cmd.Execute()
