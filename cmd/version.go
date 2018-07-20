6808bb8138238a50280f774bf9498896cb09c359
Arush Salil <me@aru.sh>
2018-07-20 16:29:28 +0530

    Add comment to version file
    Signed-off-by: Arush Salil <me@aru.sh>

--- cmd/version.go
+++ cmd/version.go
@@ -1,24 +1,26 @@
 package cmd
 
 import (
-  "fmt"
+	"fmt"
 
-  "github.com/spf13/cobra"
+	"github.com/spf13/cobra"
 )
 
 var (
+	// GITCOMMIT to be used in the version command
 	GITCOMMIT string
-	VERSION   = "dev-build"
+	// VERSION to be used in the version command
+	VERSION = "dev-build"
 )
 
 var versionCmd = &cobra.Command{
-  Use:   "version",
-  Short: "Print the version of TK8",
-  Long:  `All software has versions. This is TK8's`,
-  Run: func(cmd *cobra.Command, args []string) {
-  
-	  fmt.Println("Version:"+ VERSION + " (Build: " + GITCOMMIT +")")
-  },
+	Use:   "version",
+	Short: "Print the version of TK8",
+	Long:  `All software has versions. This is TK8's`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		fmt.Println("Version:" + VERSION + " (Build: " + GITCOMMIT + ")")
+	},
 }
 
 func init() {
