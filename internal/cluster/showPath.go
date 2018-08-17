f2f21a7bd815dee2524ecb986d08fdf5288668af
Manuel Müller <mueller.m.h@gmail.com>
2018-08-17 09:57:53 +0200

    remove unessary output, remove internal test file
    Signed-off-by: Manuel Müller <mueller.m.h@gmail.com>

--- internal/cluster/showPath.go
+++ internal/cluster/showPath.go
@@ -1,13 +1,11 @@
 package cluster
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 )
 
 func GetFilePath(fileName string) string {
 	cwd, _ := os.Getwd()
-	fmt.Println(cwd)
 	return filepath.Join(cwd, fileName)
 }
