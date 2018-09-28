20f25f5c91fd0255c941ed00a30535d09826ac32
Manuel Müller <mueller.m.h@gmail.com>
2018-09-28 16:10:41 +0200

    TK8-18|TK8-23|TK8-24 add provisioner (#53)
    add provisioner implementation
    remove flag control
    add EKS Support
    
    Signed-off-by: Manuel Müller <mueller.m.h@gmail.com>

--- internal/templates/variables.go
+++ internal/templates/variables.go
@@ -1,5 +1,64 @@
 package templates
 
+var VariablesEKS = `
+# Variables Configuration
+variable "cluster-name" {
+  default     = "{{.ClusterName}}"
+  type        = "string"
+  description = "The name of your EKS Cluster"
+}
+
+variable "aws-region" {
+  default     = "{{.AWSRegion}}"
+  # availabe regions are:
+  # us-east-1 (Virginia)
+  # us-west-2 (Oregon)
+  # eu-west-1 (Irland)
+  type        = "string"
+  description = "The AWS Region to deploy EKS"
+}
+
+variable "node-instance-type" {
+  default     = "{{.NodeInstanceType}}"
+  type        = "string"
+  description = "Worker Node EC2 instance type"
+}
+
+variable "desired-capacity" {
+  default     = {{.DesiredCapacity}}
+  type        = "string"
+  description = "Autoscaling Desired node capacity"
+}
+
+variable "max-size" {
+  default     = {{.AutoScallingMaxSize}}
+  type        = "string"
+  description = "Autoscaling maximum node capacity"
+}
+
+variable "min-size" {
+  default     = {{.AutoScallingMinSize}}
+  type        = "string"
+  description = "Autoscaling Minimum node capacity"
+}
+variable "public_key_path" {
+  default = "{{.KeyPath}}"
+  type    = "string"
+  description = "path to your eks public key"
+}
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS Access Key"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS Secret Key"
+}
+
+variable "AWS_DEFAULT_REGION" {
+  description = "AWS Region"
+}
+`
 var Variables = `
 variable "AWS_ACCESS_KEY_ID" {
   description = "AWS Access Key"
