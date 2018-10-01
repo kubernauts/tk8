3dd30b8fea7711ac339c82ac20abfa78dd9fcf5a
Manuel Müller <mueller.m.h@gmail.com>
2018-10-01 14:08:43 +0200

    FIX | TK8-32 | Remove global config
    Signed-off-by: Manuel Müller <mueller.m.h@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -52,11 +52,11 @@ func init() {
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "uses the config.yaml")
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "uses the config.yaml")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
