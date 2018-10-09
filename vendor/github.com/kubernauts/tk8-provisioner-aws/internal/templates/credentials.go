d95f198f0500523eadff9ad5310bce2a69aeae9e
Arush Salil <me@aru.sh>
2018-10-09 15:34:55 +0200

    Remove jmeter submodule, literatebee key, and empty vendor folder.
    Signed-off-by: Arush Salil <me@aru.sh>

--- vendor/github.com/kubernauts/tk8-provisioner-aws/internal/templates/credentials.go
+++
@@ -1,8 +0,0 @@
-package templates
-
-var Credentials = `
-AWS_ACCESS_KEY_ID = "{{.AwsAccessKeyID}}"
-AWS_SECRET_ACCESS_KEY = "{{.AwsSecretKey}}"
-AWS_SSH_KEY_NAME = "{{.AwsAccessSSHKey}}"
-AWS_DEFAULT_REGION = "{{.AwsDefaultRegion}}"
-`
