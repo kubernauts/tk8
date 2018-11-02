05f4e0129cb0b8c590fb26c7be550ff3b9676ff0
Manuel Müller <mueller.m.h@gmail.com>
2018-11-02 15:53:31 +0100

    move common stuff to the main pkg
    Signed-off-by: Manuel Müller <mueller.m.h@gmail.com>

--- pkg/common/main.go
+++ pkg/common/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+
+	"github.com/spf13/viper"
 )
 
 var (
@@ -69,4 +71,32 @@ func ReplaceGit(executeDir string) {
 func GetFilePath(fileName string) string {
 	cwd, _ := os.Getwd()
 	return filepath.Join(cwd, fileName)
+}
+
+// ReadViperConfigFile is define the config paths and read the configuration file.
+func ReadViperConfigFile(configName string) {
+	viper.SetConfigName(configName)
+	viper.AddConfigPath(".")
+	viper.AddConfigPath("/tk8")
+	verr := viper.ReadInConfig() // Find and read the config file.
+	ErrorCheck("no config provided", verr)
+}
+
+// AwsCredentials defines the structure to hold AWS auth credentials.
+type AwsCredentials struct {
+	AwsAccessKeyID   string
+	AwsSecretKey     string
+	AwsAccessSSHKey  string
+	AwsDefaultRegion string
+}
+
+// GetCredentials get the aws credentials from the config file.
+func GetCredentials() AwsCredentials {
+	ReadViperConfigFile("config")
+	return AwsCredentials{
+		AwsAccessKeyID:   viper.GetString("aws.aws_access_key_id"),
+		AwsSecretKey:     viper.GetString("aws.aws_secret_access_key"),
+		AwsAccessSSHKey:  viper.GetString("aws.aws_ssh_keypair"),
+		AwsDefaultRegion: viper.GetString("aws.aws_default_region"),
+	}
 }
